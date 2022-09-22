cfb4e6829f827b588223b32804ef7b77a2309793
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-09-22 16:03:43 +0900

    tailwindCSS Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    null
+    <div className='bg-red-500'>
+      <h1 className='text-black'>it works</h1>
+    </div>
   )
 }
 
